f510a159cb9bf7ea0723223633ac059c9cca781c
601754958 <601754958@qq.com>
2017-08-04 21:14:06 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/wangbin/wangbin.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056.wangbin;
+
+import org.junit.Test;
+
+/**
+ * Created by Dell on 2017/8/4.
+ */
+public class wangbin {
+
+    @Test
+    public void Test() {
+        
+    }
+}
